7bdfc61f2a3c3b3e9e67bc1e52d6f51947f8e714
Volodymyr Savchenko <Volodymyr.Savchenko@epfl.ch>
2023-07-11 16:07:47 +0200

    render tool

--- tools/gammapy/render_tool.py
+++ tools/gammapy/render_tool.py
@@ -2,10 +2,13 @@ import re
 
 tool_xml = open("gammapysim.xml").read()
 
+tool_code = open("gammapysim/gammapysim.py").read()
+tool_code = re.sub('\$', '\\$', tool_code, re.M)
+
 with open("gammapysim.xml", "w") as f:
     f.write(re.sub(
             r'<configfile name="gammapysim.py">.*?<', 
-            '<configfile name="gammapysim.py"><![CDATA[\n' + open("gammapysim/gammapysim.py").read() + '\n]]><', 
+            '<configfile name="gammapysim.py"><![CDATA[\n' + tool_code + '\n]]><', 
             tool_xml, 
             re.M | re.S
         )
